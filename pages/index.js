3b46004b9a230169fe3177ccf795d5ee35a1ab4e
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2020-11-29 11:12:50 -0600

    added typescript for backend

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>LocalCrate</title>
+        <header>Header</header>
       </Head>
 
       <main className={styles.main}>
